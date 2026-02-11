2d18621d8fc2c5d9b940b3a4cf1d3c55f35e41f4
Pradnya Ginde <pradnyaginde111@gmail.com>
2026-02-11 18:59:44 +0530

    Test3 commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,106 @@
+// import React from "react";
+// import { Card, CardMedia, CardContent, Typography, Chip } from "@mui/material";
+
+// const AlbumCard = ({ image, title, follows }) => {
+//   return (
+//     <>
+//     <Card
+//       sx={{
+//         borderRadius: 2,
+//         overflow: "hidden",
+//         backgroundColor: "#FFFF",
+//         color: "white",
+//          width: "169px",          // fills its Grid cell
+//          height: "230px",  
+//       }}
+//     >
+//       <CardMedia
+//         component="img"
+//         borderRadius= "2"
+//         image={image}
+//         alt={title}
+//         sx={{ objectFit: "cover" ,
+//             flex: 2,              // image takes 2/3 of card
+//          width: "100%",          // fills its Grid cell
+//          height: "190px", 
+//         }}
+//       />
+//       <CardContent
+//         sx={{
+//             flex: 1,
+//           display: "flex",
+//           justifyContent: "space-between",
+//           alignItems: "center",
+//           padding: "8px 12px",
+//         }}
+//       >
+        
+//         <Chip
+//           label={`${follows} Follows`}
+//           size="small"
+//           sx={{
+//             backgroundColor: "#121212",
+//             color: "#FFFF",
+//             fontWeight: 500,
+//             fontSize: "0.75rem",
+//             height: 24,
+//           }}
+//         />
+//       </CardContent>
+      
+//     </Card>
+//     <Typography
+//           variant="subtitle1"
+//           sx={{ fontSize: "0.9rem",
+//             backgroundColor: "#121212",
+//             color: "#FFFF",
+//             display: "flex",
+//             justifyContent: "flex-start",
+//            }}
+//         >
+//           {title}
+//     </Typography>
+//     </>
+    
+//   );
+// };
+
+// export default AlbumCard;
+
 import React from "react";
 import { Card, CardMedia, CardContent, Typography, Chip } from "@mui/material";
 
 const AlbumCard = ({ image, title, follows }) => {
   return (
-    <>
     <Card
       sx={{
         borderRadius: 2,
         overflow: "hidden",
         backgroundColor: "#FFFF",
-        color: "white",
-         width: "169px",          // fills its Grid cell
-         height: "230px",  
+        color: "#121212",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <CardMedia
         component="img"
-        borderRadius= "2"
         image={image}
         alt={title}
-        sx={{ objectFit: "cover" ,
-            flex: 2,              // image takes 2/3 of card
-         width: "100%",          // fills its Grid cell
-         height: "190px", 
+        sx={{
+          objectFit: "cover",
+          height: 160,
         }}
       />
       <CardContent
         sx={{
-            flex: 1,
           display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "8px 12px",
+           flexDirection: "column", 
+          //  justifyContent: "space-between",
+           alignItems: "flex-start",
+           gap: 0.5, 
+          padding: "8px 12px !important",
         }}
       >
         
@@ -46,22 +115,26 @@ const AlbumCard = ({ image, title, follows }) => {
             height: 24,
           }}
         />
+        
       </CardContent>
-      
-    </Card>
-    <Typography
+      <Typography
           variant="subtitle1"
-          sx={{ fontSize: "0.9rem",
-            backgroundColor: "#121212",
+          sx={{ fontSize: "0.9rem", 
+            backgroundColor: "#121212" , 
             color: "#FFFF",
-            display: "flex",
-            justifyContent: "flex-start",
+
+             display: "flex",
+           flexDirection: "column", 
+          //  justifyContent: "space-between",
+           alignItems: "flex-start",
+           gap: 0.5, 
+           padding: "5px"
+
            }}
         >
           {title}
-    </Typography>
-    </>
-    
+        </Typography>
+    </Card>
   );
 };
 
